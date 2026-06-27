cf482bda4686ba0c8a1f1f909d336c1e1f2307c1
omaew84-cpu <omaew84@gmail.com>
2026-06-28 00:40:38 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+self.addEventListener('install', e => { self.skipWaiting(); e.waitUntil(caches.open('shikhor-v1').then(c => c.addAll(['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'])))}); self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))) });
